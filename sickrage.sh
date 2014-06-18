72238b7aa475a299583760ece2ecabc575e703c1
Micka33 <mickael@beingenious.com>
2014-06-19 01:21:23 +0200

    Create sickrage.sh

---
+++ sickrage.sh
@@ -0,0 +1,2 @@
+#!/bin/sh                                                                                                
+exec python /sickrage/SickRage-master/SickBeard.py >> /var/log/sickrage.log 2>&1
